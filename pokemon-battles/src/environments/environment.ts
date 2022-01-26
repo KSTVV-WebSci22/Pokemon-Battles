e7aa54ec6fa82e31b1f939e20cefae051c9d4a15
Sean Block <42530814+seanblock@users.noreply.github.com>
2022-01-25 20:02:16 -0500

    firebase setup

--- pokemon-battles/src/environments/environment.ts
+++ pokemon-battles/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pokemon-battle-rpi',
+    appId: '1:352748750837:web:f99af77d02bb170ca742bc',
+    storageBucket: 'pokemon-battle-rpi.appspot.com',
+    apiKey: 'AIzaSyC2eABQ11POuOO0nf3jGR2-wjQoGrGN11g',
+    authDomain: 'pokemon-battle-rpi.firebaseapp.com',
+    messagingSenderId: '352748750837',
+  },
   production: false
 };
 
