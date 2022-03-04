34667643147625a30a2e8ede3b35348eab30285a
VNA818-RPI <Arcerv@rpi.edu>
2022-03-03 21:21:06 -0500

    conflict fixes

--- pokemon-battles-react/src/App.js
+++ pokemon-battles-react/src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/home/Home';
 import Battle from './pages/battle/Battle';
 import Welcome from './pages/welcome/Welcome';
 import Friends from './pages/friends/friends';
+import Players from './pages/players/players';
+import Party from './pages/party/party';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path='/battle' element={<Battle/>} />
         <Route path='/welcome' element={<Welcome/>} />
         <Route path='/friends' element={<Friends/>} />
+        <Route path='/players' element={<Players/>} />
+        <Route path='/party' element={<Party/>} />
       </Routes>
     </Router>
   );
