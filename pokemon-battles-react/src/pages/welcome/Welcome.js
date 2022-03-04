de06603244f6f07f6ecfb8e996ba5a76e59d154d
VNA818-RPI <Arcerv@rpi.edu>
2022-03-03 22:14:13 -0500

    conflict fixes

--- pokemon-battles-react/src/pages/welcome/Welcome.js
+++ pokemon-battles-react/src/pages/welcome/Welcome.js
@@ -3,20 +3,21 @@ import pikachu from './pikachu.png'
 import pokeball from './pokeball.png'
 import phone from './phone.png'
 import fist from './fist.png'
+import gym from '../../img/items/gym.png'
 import Sound from 'react-sound'
 import Music from '../../sounds/music/welcome.mp3'
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { ClientContext } from '../../context/ClientContext'
 
 const Welcome = () => {
-  const{mute,setMute} = useContext(ClientContext);
+  const [isPlaying, setIsPlaying] = useState(true);
+
   return (
 
   <div id="welcome" className="full-screen">
     <Sound 
       url={Music}
-      playStatus={ mute ? Sound.status.PLAYING : Sound.status.STOPPED}
+      playStatus={ isPlaying ? Sound.status.PLAYING : Sound.status.STOPPED}
     />
     <h3>Welcome back Player!</h3>
     <Link to='/battle' id="find-match" className='menu-item'>
@@ -24,21 +25,31 @@ const Welcome = () => {
       <img src={pokeball} alt="pokeball"/> 
       Random Match
     </Link>
+    <Link to='/party' id="partymenu" className='menu-item'>
+      <img src={gym} alt="pikachu"/> 
+      <img src={gym} alt="gym"/> 
+      Party
+    </Link>
     <Link to='/friends' id="friends-link" className='menu-item'>
       <img src={pikachu} alt="pikachu"/> 
       <img src={pikachu} alt="pikachu"/> 
       Friends
     </Link>
-    <div id="top-players" className='menu-item'>
-      <img src={fist} alt="phone"/> 
-      <img src={fist} alt="phone"/> 
+    <Link to='/players' id="top-players" className='menu-item'>
+      <img src={fist} alt="fist"/> 
+      <img src={fist} alt="fist"/> 
       Top Players
-    </div>
-    <Link to='/settings' id="settings" className='menu-item'>
+    </Link>
+    <Link to='/shop' id="shop-link" className='menu-item'>
+      <img src={shopImg} alt="shop"/> 
+      <img src={shopImg} alt="shop"/> 
+      Shop
+    </Link>
+    <div id="settings" className='menu-item'>
       <img src={phone} alt="phone"/> 
       <img src={phone} alt="phone"/> 
       Settings
-    </Link>
+    </div>
   </div>
   
   );
