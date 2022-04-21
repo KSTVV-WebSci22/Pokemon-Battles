adebf4f8ed5d299c3c3b9132da4147f087840ade
Sean Block <seanrblock@gmail.com>
2022-04-20 23:18:36 -0400

    Revert "Update friends.js"
    This reverts commit 851373968a9c1bc8cfd224e1302837d9666922b8.

--- pokemon-battles-react/src/pages/friends/friends.js
+++ pokemon-battles-react/src/pages/friends/friends.js
@@ -34,29 +34,28 @@ const Friends = () => {
           alert("User not found");
       }
       else{
-            console.log("user found");
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                ids.push(doc.id);
-            });
-            let myfriends = await getMyFriends(uid)
-            if(myfriends.includes(ids[0])){
-                alert("User already added");
-            }
-            else{
-                myfriends.push(ids[0]);
-                var userStatusFirestoreRef = doc(db, '/users/' + uid);
-                var uf = {
-                friends: myfriends
-                };
-                updateDoc(userStatusFirestoreRef, uf);
-                getFriendList()
-            }
+          console.log("user found");
+          querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          ids.push(doc.id);
+      });
+        let myfriends = await getMyFriends(uid)
+          if(myfriends.includes(ids[0])){
+            alert("User already added");
+           }
+           else{
+          myfriends.push(ids[0]);
+          var userStatusFirestoreRef = doc(db, '/users/' + uid);
+          var uf = {
+           friends: myfriends
+          };
+          updateDoc(userStatusFirestoreRef, uf);
+        }
      }
     
     }
 
-    const getFriendList = async () => {
+    useEffect(async () => {
         if(auth.currentUser){
             var tmpFriends = await getMyFriends(auth.currentUser.uid);
             var a = [];
@@ -67,10 +66,6 @@ const Friends = () => {
         } else {
             navigate("/");
         }
-    }
-
-    useEffect(async () => {
-        getFriendList()
     }, []);
 
 
